991c228e1fe9d3d7d6048fdf92627c930c7af84a
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2018-10-02 21:55:17 +0200

    "Commit v2 : création de l'iterator dans SortedHeap"

--- src/com/company/heap/Main.java
+++ src/com/company/heap/Main.java
@@ -1,14 +1,17 @@
 package com.company.heap;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
         //création du tableau
+
         SortedHeap H = new SortedHeap(13);
         for(int i=0;i<10;i++){
             H.insertElement(i);
         }
-        System.out.println(H.element());
+        //System.out.println(H.element());
     }
 }
--- src/com/company/heap/SortedHeap.java
+++ src/com/company/heap/SortedHeap.java
@@ -41,36 +41,39 @@ public class SortedHeap implements Heap<Integer> {
     }
 
     public boolean insertElement(Integer s) {
-        int n=1; //indice du père
-        this.ind += 1;
+        int n=0; //indice du père
         System.out.println("Je suis le dernier indice parcouru "+this.ind);
-        Itr curs = new Itr(); // crée un curseur locale qui nous permettra de parcourir l'arbre
+        Itr curs = new Itr();// crée un curseur locale qui nous permettra de parcourir l'arbre
+        //curs.next();
+        System.out.println(curs.it);
         this.queue[curs.it] = s;
-
         boolean B = true;
         while (B) { //compare avec le père
             //System.out.println("Youhou je suis dans la boucle");
             // Si l'indice du fils est impair
-            if (curs.it % 2 == 0 && curs.it>0) { //si l'indice est pair
-                n = curs.it / 2;
-                System.out.println("je suis l'indice du père et je suis "+n);
-            }/*if(curs.it==1){
-                 System.out.println("Je suis le père");
-                n=1;
-            } **/if(curs.it%2==1 && curs.it>1){// si l'indice est impair
+            if (curs.it % 2 == 0 &&curs.it>0) { //si l'indice est pair
+                n = curs.it / 2-1;
+                //System.out.println("je suis l'indice du père et je suis "+n);
+            }if(curs.it==1){
+                System.out.println("Je suis le père");
+                n=0;
+            }if(curs.it%2==1&&curs.it>1){// si l'indice est impair
                 n = (curs.it + 1) / 2-1;
-                System.out.println("je suis l'indice du père et je suis "+n);
+                //System.out.println("je suis l'indice du père et je suis "+n);
             }
             System.out.println("le père "+queue[n] +"le fils "+queue[curs.it]);
-            if (s > this.queue[n] && curs.it > 0) {//le cas où la valeur du fils est plus grand que le père
+            //System.out.println("indice père"+n+"indice fils"+curs.it);
+            if (s > this.queue[n] && curs.it >= 0) {//le cas où la valeur du fils est plus grand que le père
                 this.queue[curs.it] = this.queue[n]; // échange père fils
                 this.queue[n] = s;
-                //System.out.println("on a mis l'element "+s+"à l'endroit "+curs.it);
+                System.out.println("on a mis l'element "+s+"à l'endroit "+n);
                 curs.it = n; //on affecte l'indice du père dans la valeur de l'iterateur
-            } else {
+            }else {
                 B = false; //On sort de la boucle while
             }
+
         }
+        this.ind += 1;
         return true;
     }
    public Integer element(){
